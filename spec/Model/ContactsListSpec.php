e7b45a338776fa905599d48904e75b71298fb040
Titouan BENOIT <nightbringer4ever@gmail.com>
2017-06-22 11:42:02 +0100

    Model ContactsList spec

--- spec/Model/ContactsListSpec.php
+++ spec/Model/ContactsListSpec.php
@@ -24,4 +24,47 @@ class ContactsListSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Mailjet\MailjetBundle\Model\ContactsList');
     }
+
+    public function it_throw_error_if_bad_action()
+    {
+        $this->beConstructedWith('listid01', 'sqdkljqslkdhnkqsj', []);
+        $this->shouldThrow(new \RuntimeException("sqdkljqslkdhnkqsj: is not a valide Action."))->duringInstantiation();
+    }
+
+    public function it_can_get_listid(){
+        $this->getListId()->shouldReturn('listid01');
+    }
+
+    public function it_can_get_action(){
+        $this->getAction()->shouldReturn(ContactsList::ACTION_ADDFORCE);
+    }
+
+    public function it_can_get_contacts(){
+        $contacts = array(
+            new Contact('dede@free.fr', 'dede'),
+            new Contact('foo@bar.fr', 'foo', ['foo' => 'bar'])
+        );
+
+        $this->beConstructedWith('listid01', ContactsList::ACTION_ADDFORCE, $contacts);
+
+        $this->getContacts()->shouldReturn($contacts);
+    }
+
+    public function it_can_format_properly(){
+        $result = array(
+            'Action' => ContactsList::ACTION_ADDFORCE
+        );
+
+        $contacts = array(
+            new Contact('dede@free.fr', 'dede'),
+            new Contact('foo@bar.fr', 'foo', ['foo' => 'bar'])
+        );
+
+        $result['Contacts'] = array_map(function (Contact $contact) {
+            return $contact->format();
+        }, $contacts);
+
+
+        $this->format()->shouldReturn($result);
+    }
 }
