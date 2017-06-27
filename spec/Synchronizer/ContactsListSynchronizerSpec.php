e87111768dc0eb67b433e7e089b259e317f38eda
Titouan BENOIT <nightbringer4ever@gmail.com>
2017-06-27 16:11:49 +0200

    fix spec

--- spec/Synchronizer/ContactsListSynchronizerSpec.php
+++ spec/Synchronizer/ContactsListSynchronizerSpec.php
@@ -2,15 +2,16 @@
 
 namespace spec\Mailjet\MailjetBundle\Synchronizer;
 
+use Mailjet\MailjetBundle\Manager\ContactsListManager;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Mailjet\MailjetBundle\Client\MailjetClient;
 
 class ContactsListSynchronizerSpec extends ObjectBehavior
 {
-    public function let(MailjetClient $mailjet)
+    public function let(MailjetClient $mailjet, ContactsListManager $manager)
     {
-        $this->beConstructedWith($mailjet);
+        $this->beConstructedWith($mailjet, $manager);
     }
 
     public function it_is_initializable()
