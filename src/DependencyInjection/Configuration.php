0ce0972c3972163cabd5a7c1be4416e40b794a3d
Jonas Gielen <jonas@3gplus.be>
2021-05-04 13:30:24 +0200

    treeBuilder make sf5 compatible

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,8 +17,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mailjet');
+        $treeBuilder = new TreeBuilder('mailjet');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('mailjet');
+        }
 
         $rootNode
             ->children()
